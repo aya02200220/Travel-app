bf4cfa75b9c0b8d14ce566130334626db244438c
Aya <pyon220@gmail.com>
2023-05-29 17:22:51 -0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useContext, createContext, useEffect } from "react";
+
+import axios from "axios";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+export default App;
